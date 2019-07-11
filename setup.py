8a2fe4a704734b81650ca7457012ab5ce79e915d
Lucas Jeub <lucasjeub@gmail.com>
2019-07-11 11:20:08 +0200

    fix name metadata so version information works correctly

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='nxMultilayerNet',
+    name='nxmultilayer',
     description='NetworkX based multilayer networks',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
